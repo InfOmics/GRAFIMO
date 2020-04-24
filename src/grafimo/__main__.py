9af9537f9d71fae8f802efe714a88b3146a2c284
ManuelTgn <51021763+ManuelTgn@users.noreply.github.com>
2020-04-24 12:56:36 +0200

    Update __main__.py

--- src/grafimo/__main__.py
+++ src/grafimo/__main__.py
@@ -2,7 +2,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2020 Pinello Lab
+# Copyright (c) 2020 InfOmics
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
