9b43388832e211521866255aba5d3c33b922e417
ManuelTgn <51021763+ManuelTgn@users.noreply.github.com>
2020-12-02 15:00:56 +0100

    modified findmotif_tutorial.sh

--- tutorials/findmotif_tutorial/findmotif_tutorial.sh
+++ tutorials/findmotif_tutorial/findmotif_tutorial.sh
@@ -3,15 +3,15 @@
 #----------------------------------------------------------
 #
 # This shell script shows how to search for motif occurrences 
-# on genome variation graphs using GRAFIMO.
+# on genome variation graphs (VGs) by using GRAFIMO.
 #
-# This example search a toy motif called 'example.meme' 
+# This example searches a toy motif called 'example.meme' 
 # (see data/example.png for the motif logo) on a genome 
 # variation graph. The considered genome has two chromosomes
 # 'x' and 'y'. The corresponding VGs (XG and GBWT indexes are
 # in 'data/mygenome/' directory). 
 #
-# The motif will be searched on the regions defined in 
+# The motif will be searched on the regions defined in the
 # 'data/regions.bed' BED file. The BED file contains 86
 # regions where the motif will be searched.
 #
@@ -33,27 +33,27 @@ bedfile="data/regions.bed"
 # Note that with `-d` option we specify the directory containing
 # the VGs of chromosomes (XG and GBWT indexes)
 #
-# Note that in our VGs are encoded 2 haplotypes 
+# Note 2 haplotypes are encoded in your VGs
 grafimo findmotif -d data/mygenome/ -m $motif -b $bedfile
 
 # The results are stored in the directory 'grafimo_out_PID_MOTIFID'
 # 
-# In the directory are contained a TSV report, an HTML report and
-# a GFF3 report, which can be loaded on UCSC genome browser as 
+# A TSV report, an HTML report and a GFF3 report are contained in 
+# the directory. The GFF3 can be loaded on UCSC genome browser as 
 # custom track.
 
 
 
 # GRAFIMO computes a log-likelihood score, a P-value and a q-value
-# for each retrieved potential motif occurrence. They are weighted
-# by a background probability distribution. By default GRAFIMO 
+# for each retrieved potential motif occurrence. Suche measures are 
+# weighted by a background probability distribution. By default, GRAFIMO 
 # assumes a uniform background distribution. The user can provide
 # a custom background distribution by passing it to GRAFIMO as a text 
 # file like 'bg_nt' file in 'data/' directory. Let us assume we want 
-# tell GRAFIMO that the background distribution is the one decsribed
+# that the background distribution is the one described
 # in 'bg_nt' file and we want store the results in 'grafimo_out_bg'.
-# To do this we use `-k` and `-o` options when calling GRAFIMO from
-# command-line
+# We use `-k` and `-o` options when calling GRAFIMO from
+# command-line, in order to do this
 grafimo findmotif -d data/mygenome -m $motif -b $bedfile -k data/bg_nt -o grafimo_out_bg
 
 
@@ -79,22 +79,22 @@ grafimo findmotif -d data/mygenome -m $motif -b $bedfile --qvalueT -t 1e-4 -o gr
 
 # GRAFIMO returns only those potential motif occurrences which are 
 # observed in at least one of the haplotypes embedded in the 
-# VGs. GRAFIMO provides the user also the possibility to study the
-# recombinats that can be obtained  from the genomic variants used
-# build the VG. To do this it is sufficient to add `--recomb` option
+# VGs. GRAFIMO allows the user to study the recombinats that can be 
+# obtained  from the genomic variants used for building the VG.
+# To do so, it is sufficient to add the `--recomb` option
 # when calling GRAFIMO from command-line. Let us assume we are 
 # interested in studying all the possible recombinants obtainable from
-# the VCF variants and we want store the results in 'grafimo_out_recomb'
+# the VCF variants, and we want store the results in 'grafimo_out_recomb'
 grafimo findmotif -d data/mygenome -m $motif -b $bedfile --recomb -o grafimo_out_recomb
 
 
 
 # Let us now assume we are interested in retrieving only those motif
-# occurrences which can be found on chromosome 'x' only and we want store 
-# the results in 'grafimo_out_chrx' directory. This can be done using 
+# occurrences which can be found on chromosome 'x' and we want store 
+# the results in 'grafimo_out_chrx' directory. This can be done by using 
 # `-c` and `-o` options
 grafimo findmotif -d data/mygenome -m $motif -b $bedfile -c x -o grafimo_out_chrx
 
 
-# For more options and a deeper look see https://github.com/InfOmics/GRAFIMO/wiki
+# See https://github.com/InfOmics/GRAFIMO/wiki for additional details
 
