8b20f52738a7901151037873ca17a4ba5fc5d331
ManuelTgn <51021763+ManuelTgn@users.noreply.github.com>
2021-05-11 16:06:32 +0200

    Update travis_before_install.sh

--- .travis/travis_before_install.sh
+++ .travis/travis_before_install.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-# Install external dependendecies required to run GRAFIMO before integration 
-# testing on http://travis-ci.org/:
-# - vg
-# - tabix
-# - graphviz
+# install miniconda (Linux 64bit)
+apt-get install upgrade -y
+apt-get install curl -y
+curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh -b -p "${HOME}/miniconda"
+export PATH="${HOME}/miniconda/bin:${PATH}"
 
-# precompiled binaries are no more available for vg, so build it from source
-git clone --recursive https://github.com/vgteam/vg.git
-cd vg
-# we should already have solved VG's dependencies
-make get-deps
-# start build
-chmod +x ./source_me.sh
-. ./source_me.sh && make
+# add channels Linux
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
 
-# tabix already comes with vg
-
-# exit from vg directory and make sure it is available in the path
-cd ..
+# install VG via bioconda
+conda install vg
 vg find
 
